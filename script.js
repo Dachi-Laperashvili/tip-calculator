4e8992f4346a70e8c920463f9166606aa1494d60
Dachi <dachi.laperashvili@gmail.com>
2022-07-24 15:06:45 +0400

    added variables

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const bill = document.getElementById('bill');
+const people = document.getElementById('people');
+const tipBtn = document.querySelectorAll('.tip-btn');
+const custom = document.getElementById('custom');
+const tip = document.getElementById('tip-result');
+const total = document.getElementById('total-result');
+const reset = document.getElementById('reset');
