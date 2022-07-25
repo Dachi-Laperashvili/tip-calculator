b26f806c1ae25f564282f18c464aad4d5e83b4dc
Dachi <dachi.laperashvili@gmail.com>
2022-07-25 13:00:01 +0400

    added function to calculate tip and total

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const tip = document.getElementById('tip-result');
 const total = document.getElementById('total-result');
 const reset = document.getElementById('reset');
 
+let percentage;
 const init = () => {
   bill.value = 0;
   custom.value = '';
@@ -15,5 +16,21 @@ const init = () => {
   tip.textContent = '$0.00';
   total.textContent = '$0.00';
 };
+init();
 
+const calculateTip = function (percentage) {
+  let perPerson = bill.value / people.value;
+  let tipAmount = perPerson * (percentage / 100);
+  let totalAmount = perPerson + tipAmount;
+
+  tip.textContent = '$' + tipAmount;
+  total.textContent = '$' + totalAmount;
+};
+
+for (let i = 0; i < tipBtn.length; i++) {
+  tipBtn[i].addEventListener('click', function () {
+    let percentage = tipBtn[i].value;
+    calculateTip(percentage);
+  });
+}
 reset.addEventListener('click', init);
