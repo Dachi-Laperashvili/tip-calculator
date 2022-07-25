25c7c47d68ed8f8421e51bd589f9c2bc24a8a881
Dachi <dachi.laperashvili@gmail.com>
2022-07-25 12:38:30 +0400

    added functionality to reset button

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ const custom = document.getElementById('custom');
 const tip = document.getElementById('tip-result');
 const total = document.getElementById('total-result');
 const reset = document.getElementById('reset');
+
+const init = () => {
+  bill.value = 0;
+  custom.value = '';
+  people.value = 0;
+  tip.textContent = '$0.00';
+  total.textContent = '$0.00';
+};
+
+reset.addEventListener('click', init);
