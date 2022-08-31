e44447436f9d143a5633e88b04d806338679817c
Dachi <dachi.laperashvili@gmail.com>
2022-08-31 22:13:57 +0400

    added active state to calculator and gave and inputed functionality to custom and tip buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// variables and element selecting
+// element selecting
 
 const bill = document.getElementById('bill');
 const people = document.getElementById('people');
@@ -11,16 +11,21 @@ const total = document.getElementById('total-result');
 const reset = document.getElementById('reset');
 const error = document.getElementById('error');
 const error2 = document.getElementById('error2');
-let percentage;
+
+// variables
+let percentage, active;
 
 // function to reset every value
-
 const init = () => {
-  bill.value = 0;
+  // reseting elements value
+  active = false;
+  bill.value = '';
   custom.value = '';
-  people.value = 0;
+  people.value = '';
   tip.textContent = '$0.00';
   total.textContent = '$0.00';
+
+  // reseting styles
   error.style.display = 'none';
   error2.style.display = 'none';
   people.style.border = 'solid 2px transparent';
@@ -30,60 +35,93 @@ const init = () => {
 };
 init();
 
-// function to calculate tip and total amount
+// function to check if values are 0 or if calculator is in active state
+const checkValue = function () {
+  if (
+    people.value !== 0 &&
+    people.value !== '' &&
+    bill.value !== 0 &&
+    bill.value !== ''
+  ) {
+    active = true;
+  }
 
-const calculateTip = function (percentage) {
-  if (people.value >= 1 && bill.value >= 1) {
+  // checking if number of people value is smaller than zero or empty string
+  if (people.value <= 0 || people.value === '') {
+    error.style.display = 'block';
+    error.textContent = "Can't be zero";
+    people.style.border = 'solid 3px rgb(230, 65, 65)';
+    active = false;
+  } else {
     error.style.display = 'none';
-    error2.style.display = 'none';
     people.style.border = 'solid 2px transparent';
+    active = true;
+  }
+
+  // checking if bill value is smaller than zero or empty string
+  if (bill.value <= 0 || bill.value === '') {
+    error2.style.display = 'block';
+    error2.textContent = "Can't be zero";
+    bill.style.border = 'solid 3px rgb(230, 65, 65)';
+    active = false;
+  } else {
+    error2.style.display = 'none';
     bill.style.border = 'solid 2px transparent';
-
-    let perPerson = bill.value / people.value;
-    let tipAmount = perPerson * (percentage / 100);
-    let totalAmount = perPerson + tipAmount;
-    tip.textContent = '$' + tipAmount.toFixed(2);
-    total.textContent = '$' + totalAmount.toFixed(2);
-  }
-  if (people.value <= 0 && bill.value <= 0) {
-    error.style.display = 'block';
-    error.innerHTML = "Can't be zero";
-    error2.style.display = 'block';
-    error2.innerHTML = "Can't be zero";
-    bill.style.border = 'solid 3px rgb(230, 65, 65)';
-    people.style.border = 'solid 3px rgb(230, 65, 65)';
-  } else if (people.value <= 0) {
-    error.style.display = 'block';
-    error.innerHTML = "Can't be zero";
-    people.style.border = 'solid 3px rgb(230, 65, 65)';
-  } else if (bill.value <= 0) {
-    error2.style.display = 'block';
-    error2.innerHTML = "Can't be zero";
-    bill.style.border = 'solid 3px rgb(230, 65, 65)';
-  }
-
-  if (total.textContent !== '$0.00' && tip.textContent !== '$0.00') {
-    reset.style.backgroundColor = 'hsl(172, 67%, 45%)';
-    reset.style.color = 'hsl(183, 100%, 15%)';
+    active = true;
   }
 };
 
+// function to calculate tip and total amount
+const calculateTip = function (percentage) {
+  let perPerson = bill.value / people.value;
+  let tipAmount = perPerson * (percentage / 100);
+  let totalAmount = perPerson + tipAmount;
+
+  tip.textContent = '$' + tipAmount.toFixed(2);
+  total.textContent = '$' + totalAmount.toFixed(2);
+
+  error.style.display = 'none';
+  error2.style.display = 'none';
+  people.style.border = 'solid 2px transparent';
+  bill.style.border = 'solid 2px transparent';
+  active = false;
+};
 // looping through every tip button and giving it functionality
 
 for (let i = 0; i < tipBtn.length; i++) {
-  tipBtn[i].addEventListener('click', function () {
-    let percentage = parseFloat(tipBtn[i].value);
-    calculateTip(percentage);
-  });
+  // functionality to tip buttons
+  const tipFun = function () {
+    checkValue();
+    if (active) {
+      tipBtn[i].focus();
+      let percentage = parseFloat(tipBtn[i].value);
+      calculateTip(percentage);
+      reset.style.backgroundColor = 'hsl(172, 67%, 45%)';
+    }
+  };
+
+  // inputing tip functionality
+  tipBtn[i].addEventListener('click', tipFun);
+  bill.addEventListener('click', tipFun);
+  people.addEventListener('change', tipFun);
 }
 
 // functionality to custom button
-
-custom.addEventListener('change', function () {
-  percentage = parseFloat(custom.value);
-  calculateTip(percentage);
-});
+const customFunction = function () {
+  if (custom.value !== '') {
+    percentage = parseFloat(custom.value);
+    calculateTip(percentage);
+    reset.style.backgroundColor = 'hsl(172, 67%, 45%)';
+  }
+};
+// inputing custom functionality
+custom.addEventListener('change', customFunction);
+bill.addEventListener('change', customFunction);
+people.addEventListener('change', customFunction);
 
 // functionality to reset button
-
 reset.addEventListener('click', init);
+
+// checking if calculator is active
+bill.addEventListener('change', checkValue);
+people.addEventListener('change', checkValue);
